8fecdddc8f391675869b8808286749b94f6f3d33
Jempasam <samueldemont83@gmail.com>
2023-08-13 16:28:23 +0200

    Biome Spirit tweak, lag'a'lot, not ready

--- src/main/kotlin/jempasam/hexlink/spirit/BiomeSpirit.kt
+++ src/main/kotlin/jempasam/hexlink/spirit/BiomeSpirit.kt
@@ -1,5 +1,6 @@
 package jempasam.hexlink.spirit
 
+import jempasam.hexlink.HexlinkMod
 import net.minecraft.block.Blocks
 import net.minecraft.entity.Entity
 import net.minecraft.entity.SpawnGroup
@@ -20,34 +21,101 @@ import net.minecraft.util.registry.RegistryEntry
 import net.minecraft.util.registry.RegistryEntryList
 import net.minecraft.util.registry.RegistryKey
 import net.minecraft.world.biome.Biome
+import net.minecraft.world.gen.feature.ConfiguredFeature
 import net.minecraft.world.gen.feature.FeaturePlacementContext
 import net.minecraft.world.gen.feature.PlacedFeature
+import net.minecraft.world.gen.placementmodifier.PlacementModifierType
 import java.util.*
-import kotlin.jvm.optionals.getOrDefault
 
 
 class BiomeSpirit(val biome: RegistryEntry<Biome>): Spirit{
 
-    fun generateRandomFeature(world: ServerWorld, pos: BlockPos, features: RegistryEntryList<PlacedFeature>, rand: Random): Boolean{
-        val feature=features.get(rand.nextInt(features.size())).value()
+    companion object{
+        const val VALIDATION_SQUARE_DISTANCE=400.0f // Distance of 20
+        const val MAX_TRY_COUNT=5
+    }
+
+    fun placeAll(world: ServerWorld, center: BlockPos, positions: List<BlockPos>, feature: ConfiguredFeature<*,*>, rand: Random): Int{
+        if(positions.size==0)return 0
+
+        var spawn_count=0
+        var minx=Int.MAX_VALUE
+        var miny=Int.MAX_VALUE
+        var minz=Int.MAX_VALUE
+        var maxx=Int.MIN_VALUE
+        var maxy=Int.MIN_VALUE
+        var maxz=Int.MIN_VALUE
+        for(pos in positions){
+            if(pos.x<minx)minx=pos.x
+            if(pos.y<miny)miny=pos.y
+            if(pos.z<minz)minz=pos.z
+            if(pos.x>maxx)maxx=pos.x
+            if(pos.y>maxy)maxy=pos.y
+            if(pos.z>maxz)maxz=pos.z
+        }
+
+        val offset=Vec3i(
+                center.x-(minx+(maxx-minx)/2),
+                center.y-(miny+(maxy-miny)/2),
+                center.z-(minz+(maxz-minz)/2)
+        )
+
+        for(spawn_pos in positions){
+            val square_distance=spawn_pos.getSquaredDistance(center)
+            if(square_distance< VALIDATION_SQUARE_DISTANCE){
+                if(feature.generate(world, world.chunkManager.chunkGenerator, rand, spawn_pos.add(offset)))
+                    spawn_count++
+            }
+        }
+
+        return spawn_count
+    }
+
+    fun generateFeature(world: ServerWorld, start_pos: BlockPos, feature: PlacedFeature, rand: Random): Boolean{
         val placements=feature.placementModifiers
         val configured=feature.feature.value()
-        var final_pos=pos
-        val context=FeaturePlacementContext(world, world.chunkManager.chunkGenerator, Optional.of(feature))
-        for(placement in placements)final_pos=placement.getPositions(context, rand, final_pos).findFirst().getOrDefault(final_pos)
 
-        val distance=pos.getSquaredDistance(final_pos)
-        println("Distance is $distance")
-        if(distance<20*20){
-            configured.generate(world, world.chunkManager.chunkGenerator, rand, pos)
-            return true
+        // Get structure positions
+        var positions= listOf(start_pos)
+        val context=FeaturePlacementContext(world, world.chunkManager.chunkGenerator, Optional.of(feature))
+        for(placement in placements){
+            val newpositions=mutableListOf<BlockPos>()
+            if(placement.type!= PlacementModifierType.BIOME){
+                for(pos in positions){
+                    for(newpos in placement.getPositions(context, rand, pos)){
+                        newpositions.add(newpos)
+                    }
+                }
+                positions=newpositions
+                if(positions.size==0)break
+            }
         }
-        else return false
+
+        // Try Spawning Features
+        val spawn_count=placeAll(world, start_pos, positions, configured, rand)
+        return spawn_count>1
+    }
+
+    fun generateRandomFeature(world: ServerWorld, pos: BlockPos, features: RegistryEntryList<PlacedFeature>, rand: Random): Boolean{
+        var try_count=0
+        var selected=rand.nextInt(features.size())
+        while(try_count< MAX_TRY_COUNT){
+            val feature=features.get(selected%features.size())
+            HexlinkMod.logger.info("Try "+feature.key.get())
+            if(generateFeature(world, pos, feature.value(), rand)){
+                HexlinkMod.logger.info("Success with "+feature.key.get())
+                return true
+            }
+            selected++
+            try_count++
+        }
+        return false
     }
 
     fun generateFeatures(world: ServerWorld, position: BlockPos, number: Int, rand: Random){
         val groups=biome.value().generationSettings.features
         if(groups.size==0)return
+        var seq=0
         for(group in groups){
             if(group.size()==0)continue
             for(repetition in 0 until number){
@@ -58,25 +126,26 @@ class BiomeSpirit(val biome: RegistryEntry<Biome>): Spirit{
                         rand.nextBetween(-offset_space,offset_space+1)
                 )
                 val final_position=position.add(offset)
-                var i=0
-                while(!generateRandomFeature(world,final_position,group,rand) && i<10){
-                    i++
-                }
+                if(!generateRandomFeature(world,final_position,group,rand))
+                    HexlinkMod.logger.info("Biome Spirit generation structure n$repetition of step"+seq+" fail")
+                seq++
             }
         }
     }
 
     fun generateSnowAndIce(world: ServerWorld, position: BlockPos){
-        if(biome.value().canSetSnow(world,position)) world.setBlockState(position,Blocks.SNOW.defaultState)
-        if(biome.value().canSetIce(world,position)) world.setBlockState(position,Blocks.ICE.defaultState)
+        val finalpos=position.up()
+        if(biome.value().canSetSnow(world,finalpos)) world.setBlockState(finalpos,Blocks.SNOW.defaultState)
+        if(biome.value().canSetIce(world,finalpos)) world.setBlockState(finalpos,Blocks.ICE.defaultState)
     }
 
     fun generateMob(world: ServerWorld, position: BlockPos, number: Int, rand: Random){
+        val finalpos=position.up()
         val spawn_entries=biome.value().spawnSettings.getSpawnEntries(SpawnGroup.CREATURE).entries
         if(spawn_entries.size==0)return
         for(i in 0 until number){
             val spawn_entry=spawn_entries.get(rand.nextInt(spawn_entries.size))
-            spawn_entry.type.spawn(world,null,null,null, position, SpawnReason.SPAWNER, false, false)
+            spawn_entry.type.spawn(world,null,null,null, finalpos, SpawnReason.SPAWNER, false, false)
         }
     }
 
@@ -90,7 +159,7 @@ class BiomeSpirit(val biome: RegistryEntry<Biome>): Spirit{
 
 
     override fun infuseAtCost(caster: PlayerEntity, world: ServerWorld, position: Vec3d, power: Int): Int {
-        return if(world.getBlockState(BlockPos(position)).isAir) 1 else Spirit.CANNOT_USE
+        return if(world.getBlockState(BlockPos(position).up()).isAir) 1 else Spirit.CANNOT_USE
     }
 
     override fun infuseAt(caster: PlayerEntity, world: ServerWorld, position: Vec3d, power: Int) {
