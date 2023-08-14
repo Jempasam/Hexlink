e0907a770afdca4259f3225c12b22555c906befe
Jempasam <samueldemont83@gmail.com>
2023-08-14 19:40:11 +0200

    Vortex Crafting second steps

--- src/main/kotlin/jempasam/hexlink/block/HexVortexBlock.kt
+++ src/main/kotlin/jempasam/hexlink/block/HexVortexBlock.kt
@@ -1,7 +1,20 @@
 package jempasam.hexlink.block
 
-import net.minecraft.block.Block
+import jempasam.hexlink.entity.block.HexVortexBlockEntity
+import net.minecraft.block.BlockState
+import net.minecraft.block.BlockWithEntity
+import net.minecraft.block.entity.BlockEntity
+import net.minecraft.block.entity.BlockEntityTicker
+import net.minecraft.block.entity.BlockEntityType
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
 
-class HexVortexBlock(settings: Settings) : Block(settings) {
+class HexVortexBlock(val type: BlockEntityType<out HexVortexBlockEntity>, settings: Settings) : BlockWithEntity(settings){
+    override fun createBlockEntity(pos: BlockPos, state: BlockState): BlockEntity? {
+        return type.instantiate(pos,state)
+    }
 
+    override fun <T : BlockEntity?> getTicker(world: World?, state: BlockState?, type: BlockEntityType<T>?): BlockEntityTicker<T>? {
+        return checkType(type, this.type){w,p,s,be -> be.tick(w,p,s)}
+    }
 }
--- src/main/kotlin/jempasam/hexlink/entity/block/HexVortexBlockEntity.kt
+++ src/main/kotlin/jempasam/hexlink/entity/block/HexVortexBlockEntity.kt
@@ -1,11 +1,15 @@
 package jempasam.hexlink.entity.block
 
+import at.petrak.hexcasting.api.utils.serializeToNBT
 import jempasam.hexlink.HexlinkMod
 import jempasam.hexlink.utils.RecipeHelper
 import net.minecraft.block.BlockState
 import net.minecraft.block.entity.BlockEntity
 import net.minecraft.block.entity.BlockEntityType
 import net.minecraft.item.ItemStack
+import net.minecraft.nbt.NbtCompound
+import net.minecraft.nbt.NbtElement
+import net.minecraft.nbt.NbtList
 import net.minecraft.recipe.RecipeType
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
@@ -13,41 +17,71 @@ import net.minecraft.world.World
 class HexVortexBlockEntity(type: BlockEntityType<out HexVortexBlockEntity>, pos: BlockPos, state: BlockState) : BlockEntity(type, pos, state){
 
     companion object{
+        const val LOADING_TIME=20
+    }
 
-        fun tick(world: World, pos: BlockPos, state: BlockState, be: HexVortexBlockEntity) {
-            be.loading++
-            if(be.loading> LOADING_TIME){
-                HexlinkMod.logger.info("TICK VORTEX")
-                val inputs=be.input.toMutableList()
-                inputs.addAll(be.output)
-                val result=RecipeHelper.craft(world, RecipeType.CRAFTING, inputs)
-                if(result!=null){
-                    var toremove=result.second
-                    while(toremove>0 && be.input.size>0){
-                        be.input.removeAt(0)
-                        toremove--
-                    }
-                    while(toremove>0 && be.output.size>0){
-                        be.output.removeAt(0)
-                        toremove--
-                    }
-                    for(i in 0 until result.first.count){
-                        val out=result.first.copy()
-                        out.count=1
-                        be.output.add(out)
-                    }
+    fun tick(world: World, pos: BlockPos, state: BlockState) {
+        loading++
+        if(loading> LOADING_TIME){
+            HexlinkMod.logger.info("TICK VORTEX")
+            val inputs=input.toMutableList()
+            inputs.addAll(output)
+            val result= RecipeHelper.craft(world, RecipeType.CRAFTING, inputs)
+            if(result!=null){
+                var toremove=result.second
+                while(toremove>0 && input.size>0){
+                    input.removeAt(0)
+                    toremove--
                 }
-                be.loading=0
-                if(be.input.isEmpty()){
-                    world.removeBlock(pos,false)
+                while(toremove>0 && output.size>0){
+                    output.removeAt(0)
+                    toremove--
+                }
+                for(i in 0 until result.first.count){
+                    val out=result.first.copy()
+                    out.count=1
+                    output.add(out)
                 }
             }
+            loading=0
+            if(input.isEmpty()){
+                world.removeBlock(pos,false)
+            }
         }
+        markDirty()
+    }
 
-        const val LOADING_TIME=20
+    fun give(stack: ItemStack){
+        for(i in 0 until stack.count){
+            val out=stack.copy()
+            out.count=1
+            output.add(out)
+        }
+        loading=0
+        markDirty()
+    }
+
+    override fun writeNbt(nbt: NbtCompound) {
+        nbt.putInt("loading", loading)
+        val input_nbt=NbtList()
+        val output_nbt=NbtList()
+        for(item in input)input_nbt.add(item.serializeToNBT())
+        for(item in output)output_nbt.add(item.serializeToNBT())
+        nbt.put("input",input_nbt)
+        nbt.put("output",output_nbt)
+    }
+
+    override fun readNbt(nbt: NbtCompound) {
+        loading=nbt.getInt("loading")
+        nbt.getList("input",NbtElement.COMPOUND_TYPE.toInt()).forEach{
+            if(it is NbtCompound)input.add(ItemStack.fromNbt(it))
+        }
+        nbt.getList("output",NbtElement.COMPOUND_TYPE.toInt()).forEach{
+            if(it is NbtCompound)output.add(ItemStack.fromNbt(it))
+        }
     }
 
     private var loading=0
     private val input= mutableListOf<ItemStack>()
-    private val output=mutableListOf<ItemStack>()
+    private val output= mutableListOf<ItemStack>()
 }
